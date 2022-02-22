bb43ae9dd867819ab40ffbd34f2942c5ce40d109
janhavi <janashinde394@gmail.com>
2022-02-22 22:36:41 +1100

    finished challenge successfully

---
+++ index.js
@@ -0,0 +1,42 @@
+function add(num1, num2) { 
+return num1 + num2
+}
+
+add(4,10);
+
+function subtract(num1, num2) {
+return num1 - num2
+}
+subtract(10,5);
+
+function multiply(num1, num2) {
+return num1 * num2
+}
+multiply(6,2);
+
+function divide(num1, num2) {
+return num1/num2
+}
+divide(10,2);
+
+function increment(n) {
+    return ++n
+}
+increment(866);
+
+function decrement(n) {
+    return --n
+}
+decrement(10)
+
+
+
+function makeInt (n) {
+    return parseInt(n,10)
+}
+makeInt("2");
+
+function preserveDecimal(n) {
+    return parseFloat(n)
+}
+preserveDecimal(2.222);
